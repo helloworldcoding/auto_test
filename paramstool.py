b8ee93e7fb8f2853ba58fcd0138324154d9c565e
mingazi <mingazi@163.com>
2019-09-10 07:46:27 +0800

    paramstool.py

--- paramstool.py
+++ paramstool.py
@@ -5,22 +5,23 @@ import itertools
 def full_arrangement(**kwargs):
     print(kwargs)
     print(kwargs.values())
-    for x in itertools.product(kwargs):
-        print(x)
+    result = [[]]
+    for val in kwargs.values():
+        result = [x+[y] for x in result for y in val]
+    print(result)
+    #mapRes = [[]]
+    mapRes = [{}]
+    for k,v in kwargs.items():
+       # mapRes = [_x+[{k:_y}] for _x in mapRes for _y in v]
+        mapRes = [_x  if _x and (_x.update({k:_y}) is None)  else {k:_y}  for _x in mapRes for _y in v]
+    print(mapRes,len(mapRes))
+    #for x in itertools.product(kwargs):
+    #    print(x)
 
 def _test_full_arrangement():
-    full_arrangement(name=["zhangsan"],age=[12,23],status=[1,2,3,5])
-    full_arrangement(name=("zhangsan"),age=(12,23),status=(1,2,3,5))
+    full_arrangement(name=["zhangsan"],age=[12,23,2],status=[1,2,3,5])
     
-def product(*args, **kwds):
-    # product('ABCD', 'xy') --> Ax Ay Bx By Cx Cy Dx Dy
-    # product(range(2), repeat=3) --> 000 001 010 011 100 101 110 111
-    pools = map(tuple, args) * kwds.get('repeat', 1)
-    result = [[]]
-    for pool in pools:
-        result = [x+[y] for x in result for y in pool]
-    for prod in result:
-        yield tuple(prod)    
-    
+   
 if __name__ == '__main__':
     _test_full_arrangement()
+
